052d6f791080e13523678063516ea3be05ca261f
Andy Eccleston <eandy5000@yahoo.com>
2017-02-27 10:00:26 -0600

    basic react config with app file working

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Days of the week</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
